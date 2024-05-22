866e3f3e7b56ed92c77209fe5adb1150be48d4f0
GemGemMin <kkm32543@khu.ac.kr>
2024-05-22 15:46:48 +0900

    solve 1764

--- BOJ/sorting/1764.cpp
+++ BOJ/sorting/1764.cpp
@@ -5,41 +5,30 @@ using namespace std;
 
 priority_queue<string, vector<string>, greater<>> pq;
 int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+
     int N, M;
     cin >> N >> M;
-    for (int i = 0; i < N; i++) {
+    for (int i = 0; i < N + M; i++) {
         string str;
         cin >> str;
         pq.push(str);
     }
+    queue<string> q;
+    int size = pq.size();
+    for (int i = 0; i < size-1; i++) {
+        string s = pq.top();
+        pq.pop();
+        if (s == pq.top()) {
+            q.push(s);
+        }
+    }
+    cout << q.size() << "\n";
+    while (!q.empty()) {
+        cout << q.front() << "\n";
+        q.pop();
+    }
+
 }
-
-
-//#include<iostream>
-//#include<algorithm>
-//#include<string>
-//#include<vector>
-//#include<set>
-//using namespace std;
-//
-//vector<string> sum_v;
-//int main(){
-//    int N, M;
-//    set<string> s;
-//    cin >> N >> M;
-//    for (int i = 0; i < N; i++) {
-//        string str;
-//        cin >> str;
-//        s.insert(str);
-//        sum_v.push_back(str);
-//    }
-//    for (int i = 0; i < M; i++) {
-//        string str;
-//        cin >> str;
-//        s.insert(str);
-//        sum_v.push_back(str);
-//    }
-//    sort(sum_v.begin(), sum_v.end(), greater<>());
-//
-//
-//}
