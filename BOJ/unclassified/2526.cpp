a1f634b1d423af51a78ab823e1dd4681a115609d
GemGemMin <kkm32543@khu.ac.kr>
2024-06-30 23:59:53 +0900

    solve 2526

--- BOJ/unclassified/2526.cpp
+++ BOJ/unclassified/2526.cpp
@@ -8,10 +8,8 @@ int main()
 {
     cin >> n >> p;
     int cnt = 0;
-    //계산함수
     func(n);
 
-    //check[]가 2인 갯수 출력
     for (int i = 0; i <p; i++)
     {
         if (check[i] == 2)cnt++;
@@ -20,8 +18,6 @@ int main()
 }
 void func(int x)
 {
-    //한번 나온값이 또 나오면?
-    //또 나올 떄 까지의 과정들이 싸이클임
     if (check[x] == 2)return;
     check[x]++;
     func(x * n % p);
